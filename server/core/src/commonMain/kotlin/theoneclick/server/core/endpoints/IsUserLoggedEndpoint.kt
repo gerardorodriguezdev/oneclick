5c025aa71909d66c6ca10b0e77e8ba05c4057780
Gerardo Rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-03-27 11:30:55 +0000

    Updates auth on server

--- server/core/src/commonMain/kotlin/theoneclick/server/core/endpoints/IsUserLoggedEndpoint.kt
+++ server/core/src/commonMain/kotlin/theoneclick/server/core/endpoints/IsUserLoggedEndpoint.kt
@@ -3,18 +3,15 @@ package theoneclick.server.core.endpoints
 import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.server.sessions.*
-import theoneclick.server.core.extensions.agent
 import theoneclick.server.core.extensions.defaultAuthentication
 import theoneclick.server.core.models.UserSession
-import theoneclick.shared.core.models.agents.Agent
 import theoneclick.shared.core.models.endpoints.ClientEndpoint
 import theoneclick.shared.core.models.responses.UserLoggedResponse
 
 fun Routing.isUserLoggedEndpoint() {
     defaultAuthentication(optional = true) {
         get(ClientEndpoint.IS_USER_LOGGED.route) {
-            val userSession = userSession()
+            val userSession = call.principal<UserSession>()
 
             if (userSession == null) {
                 call.respond<UserLoggedResponse>(UserLoggedResponse.NotLogged)
@@ -24,9 +21,3 @@ fun Routing.isUserLoggedEndpoint() {
         }
     }
 }
-
-private fun RoutingContext.userSession(): UserSession? =
-    when (call.request.agent) {
-        Agent.MOBILE -> call.principal<UserSession>()
-        Agent.BROWSER -> call.sessions.get<UserSession>() //TODO: Is this ok or should be principal?
-    }
