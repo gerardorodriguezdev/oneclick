a272684cab5a46a90a9bf94b12996fbe054c0148
gerardo.rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-07-11 16:23:56 +0100

    Adds first iOS commits

---
+++ client/app/src/iosMain/kotlin/theoneclick/client/app/MainViewController.kt
@@ -0,0 +1,19 @@
+package theoneclick.client.app
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.ComposeUIViewController
+import platform.UIKit.UIViewController
+
+fun MainViewController(): UIViewController = ComposeUIViewController { App() }
+
+@Composable
+private fun App() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(text = "Text")
+    }
+}
