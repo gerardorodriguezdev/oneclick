ebcbb5967c12311617f68697d7075a2a0b762479
gerardo.rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-08-06 18:36:38 +0100

    Finishes iOS implementation

---
+++ client/shared/di/src/iosMain/kotlin/theoneclick.client.shared.di/IOSCoreComponent.kt
@@ -0,0 +1,49 @@
+package theoneclick.client.shared.di
+
+import io.ktor.client.engine.*
+import io.ktor.http.*
+import theoneclick.client.shared.navigation.NavigationController
+import theoneclick.client.shared.network.dataSources.TokenDataSource
+import theoneclick.client.shared.network.platform.IOSRemoteAuthenticationDataSource
+import theoneclick.client.shared.network.platform.LogoutManager
+import theoneclick.client.shared.network.platform.iosHttpClient
+import theoneclick.client.shared.notifications.NotificationsController
+import theoneclick.shared.dispatchers.platform.DispatchersProvider
+import theoneclick.shared.logging.AppLogger
+
+fun iosCoreComponent(
+    urlProtocol: URLProtocol?,
+    host: String?,
+    port: Int?,
+    httpClientEngine: HttpClientEngine,
+    tokenDataSource: TokenDataSource,
+    appLogger: AppLogger,
+    dispatchersProvider: DispatchersProvider,
+    navigationController: NavigationController,
+    logoutManager: LogoutManager,
+    notificationsController: NotificationsController,
+): CoreComponent {
+    val httpClient = iosHttpClient(
+        urlProtocol = urlProtocol,
+        host = host,
+        port = port,
+        appLogger = appLogger,
+        httpClientEngine = httpClientEngine,
+        tokenDataSource = tokenDataSource,
+        logoutManager = logoutManager,
+    )
+
+    return CoreComponent::class.create(
+        appLogger = appLogger,
+        dispatchersProvider = dispatchersProvider,
+        navigationController = navigationController,
+        httpClient = httpClient,
+        authenticationDataSource = IOSRemoteAuthenticationDataSource(
+            httpClient,
+            dispatchersProvider,
+            tokenDataSource,
+            appLogger
+        ),
+        notificationsController = notificationsController,
+    )
+}
