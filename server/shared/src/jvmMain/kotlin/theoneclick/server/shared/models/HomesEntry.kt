7c49271dd9989fb9af4c458aa49acd976324d1af
gerardo.rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-08-06 10:18:20 +0100

    Removes deserialization

--- server/shared/src/jvmMain/kotlin/theoneclick/server/shared/models/HomesEntry.kt
+++ server/shared/src/jvmMain/kotlin/theoneclick/server/shared/models/HomesEntry.kt
@@ -1,11 +1,9 @@
 package theoneclick.server.shared.models
 
-import kotlinx.serialization.Serializable
 import theoneclick.shared.contracts.core.models.Home
 import theoneclick.shared.contracts.core.models.UniqueList
 import theoneclick.shared.contracts.core.models.Uuid
 
-@Serializable
 data class HomesEntry(
     val userId: Uuid,
     val homes: UniqueList<Home>,
