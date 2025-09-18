dfeac36c62ececc2e07a87f97a15a1775354514c
gerardorodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-09-18 17:53:15 +0100

    Adds formatting verification HashedPassword.kt

--- server/shared/auth/src/jvmMain/kotlin/theoneclick/server/shared/auth/models/HashedPassword.kt
+++ server/shared/auth/src/jvmMain/kotlin/theoneclick/server/shared/auth/models/HashedPassword.kt
@@ -3,11 +3,16 @@ package theoneclick.server.shared.auth.models
 import dev.drewhamilton.poko.Poko
 import kotlinx.serialization.Serializable
 
-//TODO: Formatting here
 @Serializable
 @Poko
 class HashedPassword private constructor(val value: String) {
     companion object {
+        private const val ERROR_MESSAGE = "Invalid hashed password"
+
+        private val REGEX = "^\$2[ayb]\$[0-9]{2}\$[A-Za-z0-9./]{53}$".toRegex()
+
+        private fun isValid(value: String): Boolean = REGEX.matches(value)
+
         fun unsafe(value: String): HashedPassword = HashedPassword(value)
     }
 }
