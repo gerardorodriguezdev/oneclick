a1ee6312013273bd596178e749d90cdf36eda3f5
gerardorodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-10-21 19:09:47 +0100

    Fixes logout manager

--- client/shared/network/src/commonMain/kotlin/oneclick/client/shared/network/platform/LogoutManager.kt
+++ client/shared/network/src/commonMain/kotlin/oneclick/client/shared/network/platform/LogoutManager.kt
@@ -1,20 +1,5 @@
 package oneclick.client.shared.network.platform
 
-import oneclick.client.shared.navigation.NavigationController
-import oneclick.client.shared.navigation.NavigationController.NavigationEvent
-import oneclick.client.shared.navigation.models.routes.AppRoute
-import oneclick.client.shared.navigation.popUpToInclusive
-
 interface LogoutManager {
     suspend fun logout()
-
-    suspend fun NavigationController.logout() {
-        sendNavigationEvent(
-            NavigationEvent.Navigate(
-                destinationRoute = AppRoute.Login,
-                launchSingleTop = true,
-                popUpTo = popUpToInclusive(startRoute = AppRoute.Home)
-            )
-        )
-    }
 }
