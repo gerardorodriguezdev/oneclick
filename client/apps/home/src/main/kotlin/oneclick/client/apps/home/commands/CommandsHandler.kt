9fa0d04097eb27dbfc1e27387b59a06092672966
gerardorodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-10-29 10:00:13 +0000

    Updates commands parser

--- client/apps/home/src/main/kotlin/oneclick/client/apps/home/commands/CommandsHandler.kt
+++ client/apps/home/src/main/kotlin/oneclick/client/apps/home/commands/CommandsHandler.kt
@@ -1,6 +1,5 @@
 package oneclick.client.apps.home.commands
 
-import oneclick.client.apps.home.DevicesController
 import oneclick.client.shared.network.models.LogoutResult
 import oneclick.client.shared.network.models.RequestLoginResult.Error
 import oneclick.client.shared.network.models.RequestLoginResult.ValidLogin
@@ -8,7 +7,6 @@ import oneclick.client.shared.network.platform.AuthenticationDataSource
 import oneclick.shared.contracts.auth.models.Password
 import oneclick.shared.contracts.auth.models.Username
 import oneclick.shared.contracts.auth.models.requests.RequestLoginRequest
-import oneclick.shared.contracts.core.models.Uuid
 import oneclick.shared.logging.AppLogger
 
 internal interface CommandsHandler {
@@ -17,27 +15,18 @@ internal interface CommandsHandler {
     sealed interface Command {
         data class Login(val username: Username, val password: Password) : Command
         data object Logout : Command
-        data object Scan : Command
-        data class Connect(val id: Uuid, val password: Password) : Command
-        data class Disconnect(val id: Uuid) : Command
-        data class Remove(val id: Uuid) : Command
     }
 }
 
 internal class DefaultCommandsHandler(
     private val authenticationDataSource: AuthenticationDataSource,
     private val logger: AppLogger,
-    private val devicesController: DevicesController,
 ) : CommandsHandler {
 
     override suspend fun execute(command: CommandsHandler.Command) {
         when (command) {
             is CommandsHandler.Command.Login -> command.handle()
             is CommandsHandler.Command.Logout -> command.handle()
-            is CommandsHandler.Command.Scan -> command.handle()
-            is CommandsHandler.Command.Connect -> command.handle()
-            is CommandsHandler.Command.Disconnect -> command.handle()
-            is CommandsHandler.Command.Remove -> command.handle()
         }
     }
 
@@ -61,47 +50,6 @@ internal class DefaultCommandsHandler(
         when (result) {
             is LogoutResult.Success -> logger.i("Logout successful")
             is LogoutResult.Error -> logger.e("Logout failed")
-        }
-    }
-
-    private suspend fun CommandsHandler.Command.Scan.handle() {
-        val devices = devicesController.scan()
-        if (devices.isEmpty()) {
-            logger.e("No devices found")
-        } else {
-            devices.forEach { device ->
-                logger.i("Device found with id: ${device.value}")
-            }
-        }
-    }
-
-    private suspend fun CommandsHandler.Command.Connect.handle() {
-        val connectedResult = devicesController.connect(
-            id = id,
-            password = password,
-        )
-        if (connectedResult) {
-            logger.i("Device connected")
-        } else {
-            logger.e("Error connecting device")
-        }
-    }
-
-    private suspend fun CommandsHandler.Command.Disconnect.handle() {
-        val disconnectedResult = devicesController.disconnect(id = id)
-        if (disconnectedResult) {
-            logger.i("Device disconnected")
-        } else {
-            logger.e("Error disconnecting device")
-        }
-    }
-
-    private suspend fun CommandsHandler.Command.Remove.handle() {
-        val removedResult = devicesController.remove(id = id)
-        if (removedResult) {
-            logger.i("Device removed")
-        } else {
-            logger.e("Error removing device")
         }
     }
 }
--- client/apps/home/src/main/kotlin/oneclick/client/apps/home/commands/CommandsParser.kt
+++ client/apps/home/src/main/kotlin/oneclick/client/apps/home/commands/CommandsParser.kt
@@ -6,19 +6,13 @@ import oneclick.client.apps.home.commands.CommandsParser.CommandParserResult.Suc
 import oneclick.client.apps.home.models.Entry
 import oneclick.shared.contracts.auth.models.Password.Companion.toPassword
 import oneclick.shared.contracts.auth.models.Username.Companion.toUsername
-import oneclick.shared.contracts.core.models.Uuid.Companion.toUuid
 
 internal object CommandsParser {
     private const val LOGIN_KEY = "login"
     private const val LOGOUT_KEY = "logout"
-    private const val SCAN_KEY = "scan"
-    private const val CONNECT_KEY = "connect"
-    private const val DISCONNECT_KEY = "disconnect"
-    private const val REMOVE_KEY = "remove"
 
     private const val USERNAME_ARG = "username"
     private const val PASSWORD_ARG = "password"
-    private const val DEVICE_ID_ARG = "id"
 
     private const val ARG_SEPARATOR = "--"
     private const val ARG_VALUE_SEPARATOR = "="
@@ -51,22 +45,6 @@ internal object CommandsParser {
             }
 
             LOGOUT_KEY -> Command.Logout.toSuccess()
-            SCAN_KEY -> Command.Scan.toSuccess()
-            CONNECT_KEY -> {
-                val id = argumentsEntries[DEVICE_ID_ARG]?.value?.toUuid() ?: return Error("Invalid id")
-                val password = argumentsEntries[PASSWORD_ARG]?.value?.toPassword() ?: return Error("Invalid password")
-                Command.Connect(id = id, password = password).toSuccess()
-            }
-
-            DISCONNECT_KEY -> {
-                val id = argumentsEntries[DEVICE_ID_ARG]?.value?.toUuid() ?: return Error("Invalid id")
-                Command.Disconnect(id = id).toSuccess()
-            }
-
-            REMOVE_KEY -> {
-                val id = argumentsEntries[DEVICE_ID_ARG]?.value?.toUuid() ?: return Error("Invalid id")
-                Command.Remove(id = id).toSuccess()
-            }
 
             else -> Error("No command key found")
         }
