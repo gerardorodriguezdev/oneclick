278f16050657ea0d60b91c2dd0008b4c30222d70
gerardorodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-11-26 14:21:16 +0000

    Removes running docker image on mac

--- build-logic/convention/src/main/kotlin/buildLogic/convention/plugins/JvmAppPlugin.kt
+++ build-logic/convention/src/main/kotlin/buildLogic/convention/plugins/JvmAppPlugin.kt
@@ -13,11 +13,9 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.ApplicationPlugin
 import org.gradle.api.plugins.JavaApplication
-import org.gradle.api.tasks.Exec
 import org.gradle.api.tasks.JavaExec
 import org.gradle.api.tasks.bundling.Jar
 import org.gradle.kotlin.dsl.named
-import org.gradle.kotlin.dsl.register
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 
@@ -31,7 +29,7 @@ class JvmAppPlugin : Plugin<Project> {
             configureJavaApplicationExtension(jvmAppExtension)
             configureKotlinExtension(jvmAppExtension)
             configureJibExtension(jvmAppExtension)
-            registerTasks(jvmAppExtension = jvmAppExtension, chamaleonExtension = chamaleonExtension)
+            registerTasks(chamaleonExtension)
         }
     }
 
@@ -86,7 +84,8 @@ class JvmAppPlugin : Plugin<Project> {
                 }
 
                 from {
-                    image = "eclipse-temurin:${jvmAppExtension.jvmTarget.get()}-jre"
+                    val jvmTarget = jvmAppExtension.jvmTarget.get()
+                    image = "gcr.io/distroless/java$jvmTarget-debian13:latest-arm64"
                 }
 
                 to {
@@ -116,7 +115,7 @@ class JvmAppPlugin : Plugin<Project> {
         }
     }
 
-    private fun Project.registerTasks(jvmAppExtension: JvmAppExtension, chamaleonExtension: ChamaleonExtension?) {
+    private fun Project.registerTasks(chamaleonExtension: ChamaleonExtension?) {
         val environmentVariablesProvider = provider { chamaleonExtension.toMap() }
         tasks.named<JavaExec>(RUN_TASK_NAME) {
             standardInput = System.`in`
@@ -129,50 +128,13 @@ class JvmAppPlugin : Plugin<Project> {
             }
         }
 
-        val jibBuildTarTask = tasks.named(JibPlugin.BUILD_DOCKER_TASK_NAME)
-        tasks.register<Exec>(RUN_DOCKER_TASK_NAME) {
-            dependsOn(jibBuildTarTask)
-
-            standardInput = System.`in`
-            standardOutput = System.out
-            errorOutput = System.err
-
-            commandLine(
-                "bash",
-                "-c",
-                runDockerCommand(
-                    environmentVariables = environmentVariablesProvider.get(),
-                    port = jvmAppExtension.dockerConfiguration.port.get().toString(),
-                    fullNameDockerImage = fullNameDockerImage(
-                        imageRegistryUrl = jvmAppExtension.dockerConfiguration.registryUrl,
-                        imageName = jvmAppExtension.dockerConfiguration.name,
-                        identifier = provider { "latest" },
-                    ).get(),
-                )
-            )
-        }
-
         tasks.register(PUBLISH_IMAGE_TASK_NAME) {
             dependsOn(JibPlugin.BUILD_IMAGE_TASK_NAME)
         }
     }
 
-    private fun runDockerCommand(
-        environmentVariables: Map<String, String>,
-        port: String,
-        fullNameDockerImage: String,
-    ): String {
-        val environmentVariables = environmentVariables.entries
-        val environmentVariablesStrings = environmentVariables.map { (key, value) -> "$key=$value" }
-        val environmentVariablesString =
-            environmentVariablesStrings.joinToString(prefix = " ", separator = " ", transform = { "-e $it" })
-
-        return "docker run -i -p $port:$port$environmentVariablesString $fullNameDockerImage"
-    }
-
     private companion object {
         const val RUN_TASK_NAME = "run"
-        const val RUN_DOCKER_TASK_NAME = "runDocker"
         const val PUBLISH_IMAGE_TASK_NAME = "publishImage"
         const val JVM_JAR_TASK_NAME = "jar"
         const val JVM_APP_EXTENSION_NAME = "jvmApp"
